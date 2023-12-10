e42d7e89443fdf3f140d41d59b7879a66607a79b
Warren <99427649+kobe-eth@users.noreply.github.com>
2023-12-10 20:00:04 +0100

    feat: initial commit

---
+++ src/lockers/Lockers.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0;
+
+library CRV {
+    address public constant TOKEN = 0xD533a949740bb3306d119CC777fa900bA034cd52;
+    address public constant SDTOKEN = 0xD1b5651E55D4CeeD36251c61c50C889B36F6abB5;
+    address public constant LOCKER = 0x52f541764E6e90eeBc5c21Ff570De0e2D63766B6;
+    address public constant DEPOSITOR = 0xc1e3Ca8A3921719bE0aE3690A0e036feB4f69191;
+    address public constant GAUGE = 0x7f50786A0b15723D741727882ee99a0BF34e3466;
+    address public constant ACCUMULATOR = 0xa44bFD194Fd7185ebecEcE4F7fA87a47DaA01c6A;
+}
+
+library ANGLE {
+    address public constant TOKEN = 0x31429d1856aD1377A8A0079410B297e1a9e214c2;
+    address public constant SDTOKEN = 0x752B4c6e92d96467fE9b9a2522EF07228E00F87c;
+    address public constant LOCKER = 0xD13F8C25CceD32cdfA79EB5eD654Ce3e484dCAF5;
+    address public constant DEPOSITOR = 0x8A97e8B3389D431182aC67c0DF7D46FF8DCE7121;
+    address public constant GAUGE = 0xE55843a90672f7d8218285e51EE8fF8E233F35d5;
+    address public constant ACCUMULATOR = 0x8cc02F4f383A11b989708437DbA6BB0628d7eE78;
+}
+
+library SPECTRA {
+    address public constant TOKEN = 0x4104b135DBC9609Fc1A9490E61369036497660c8;
+    address public constant SDTOKEN = 0x26f01FE3BE55361b0643bc9d5D60980E37A2770D;
+    address public constant LOCKER = 0xE71e28a510bC3F98a9E77e847aE5AEF9a2e5721d;
+    address public constant DEPOSITOR = 0xFe928ca6a9C0cdf658a26A374b7373B9D6CefBCf;
+    address public constant GAUGE = 0x9c9d06C7378909C6d0A2A0017Bb409F7fb8004E0;
+    address public constant ACCUMULATOR = 0xc50f67DB3a63641a57d2d3DE9FdA6767E999Efe5;
+}
+
+library BALANCER {
+    address public constant TOKEN = 0x5c6Ee304399DBdB9C8Ef030aB642B10820DB8F56;
+    address public constant SDTOKEN = 0xF24d8651578a55b0C119B9910759a351A3458895;
+    address public constant LOCKER = 0xea79d1A83Da6DB43a85942767C389fE0ACf336A5;
+    address public constant DEPOSITOR = 0x3e0d44542972859de3CAdaF856B1a4FD351B4D2E;
+    address public constant GAUGE = 0x3E8C72655e48591d93e6dfdA16823dB0fF23d859;
+    address public constant ACCUMULATOR = 0x99e8cBa4e91aDeA2C9321344e33FCCCBfBA9b0C4;
+}
+
