7e84fff73e131fd40439c3fbe13a8fc5fe09ca93
Warren <99427649+kobe-eth@users.noreply.github.com>
2023-12-11 15:13:50 +0100

    feat: add couple protocols wip

--- src/protocols/1.sol
+++ src/protocols/1.sol
@@ -16,8 +16,8 @@ library Frax {
     address public constant FXS = 0x3432B6A60D23Ca0dFCa7761B7ab56459D9C964D0;
     address public constant FPIS = 0xc2544A32872A91F4A553b404C6950e89De901fdb;
 
-    address public constant VE_FXS = 0xc8418aF6358FFddA74e09Ca9CC3Fe03Ca6aDC5b0;
-    address public constant VE_FPIS = 0x574C154C83432B0A45BA3ad2429C3fA242eD7359;
+    address public constant VEFXS = 0xc8418aF6358FFddA74e09Ca9CC3Fe03Ca6aDC5b0;
+    address public constant VEFPIS = 0x574C154C83432B0A45BA3ad2429C3fA242eD7359;
 
     address public constant FRAX = 0x853d955aCEf822Db058eb8505911ED77F175b99e;
 
@@ -32,7 +32,7 @@ library Yearn {
     address public constant YFI = 0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e;
     address public constant DYFI = 0x41252E8691e964f7DE35156B68493bAb6797a275;
 
-    address public constant VE_YFI = 0x90c1f9220d90d3966FbeE24045EDd73E1d588aD5;
+    address public constant VEYFI = 0x90c1f9220d90d3966FbeE24045EDd73E1d588aD5;
 
     address public constant YFI_REWARD_POOL = 0xb287a1964AEE422911c7b8409f5E5A273c1412fA;
     address public constant DYFI_REWARD_POOL = 0x2391Fc8f5E417526338F5aa3968b1851C16D894E;
@@ -40,7 +40,21 @@ library Yearn {
 
 library Pendle {
     address public constant PENDLE = 0x808507121B80c02388fAd14726482e061B8da827;
-    address public constant VE_PENDLE = 0x4f30A9D41B80ecC5B94306AB4364951AE3170210;
+    address public constant VEPENDLE = 0x4f30A9D41B80ecC5B94306AB4364951AE3170210;
 
     address public constant FEE_DISTRIBUTOR = 0x8C237520a8E14D658170A633D96F8e80764433b9;
+}
+
+library Maverick {
+    address public constant MAV = 0x7448c7456a97769F6cD04F1E83A4a23cCdC46aBD;
+    address public constant VEMAV = 0x4949Ac21d5b2A0cCd303C20425eeb29DCcba66D8;
+}
+
+library Fx {
+    address public constant FXN = 0x365AccFCa291e7D3914637ABf1F7635dB165Bb09;
+    address public constant VEFXN = 0xEC6B8A3F3605B083F7044C0F31f2cac0caf1d469;
+
+    address public constant FEE_DISTRIBUTOR = 0x851AAEA3A2757D457E1Ce88C3808C1690213e432;
+    address public constant GAUGE_CONTROLLER = 0xe60eB8098B34eD775ac44B1ddE864e098C6d7f37;
+    address public constant SMART_WALLET_CHECKER = 0xD71B8B76015F296E53D41e8288a8a13eAfFff2ea;
 }
