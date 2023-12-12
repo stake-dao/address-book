b4bcc86be28511c5a65b0b221972aa1d910ce895
PumpkingWok <andrearanma@gmail.com>
2023-12-12 11:14:18 +0100

    feat: strategy addresses

---
+++ src/strategies/1.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity >=0.6.0;
+
+library CRV {
+    address public constant STRATEGY = 0x20F1d4Fed24073a9b9d388AfA2735Ac91f079ED6;
+}
+
+library ANGLE {
+    address public constant STRATEGY = 0x22635427C72e8b0028FeAE1B5e1957508d9D7CAF;
+}
+
+library BAL {
+    address public constant STRATEGY = 0x873b031Ea6E4236E44d933Aae5a66AF6d4DA419d;
+}
+
+library FXS {
+    address public constant STRATEGY = 0xf285Dec3217E779353350443fC276c07D05917c3;
+}
+
+library YFI {
+    address public constant STRATEGY = 0x1be150a35bb8233d092747eBFDc75FB357c35168;
+}
+
+library PENDLE {
+    address public constant STRATEGY = 0xA7641acBc1E85A7eD70ea7bCFFB91afb12AD0c54;
+}
---
+++ src/strategies/42161.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity >=0.6.0;
+
+library CRV {
+    address public constant STRATEGY = 0x2B82FB2B4bac16a1188f377D6a913f235715031b;
+}
