8053a7f849154c2b2648cdfe699f893a88ade1cb
PumpkingWok <andrearanma@gmail.com>
2023-12-20 15:30:06 +0100

    feat: vm platform addresses

---
+++ src/vm/1.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity >=0.6.0;
+
+library CRV {
+    address public constant PLATFORM = 0x0000000895cB182E6f983eb4D8b4E0Aa0B31Ae4c;
+}
+
+library ANGLE {
+    address public constant PLATFORM = 0x00000004E4FB0C3017b543EF66cC8A89F5dE74Ff;
+}
+
+library BAL {
+    address public constant PLATFORM = 0x0000000446b28e4c90DbF08Ead10F3904EB27606;
+}
+
+library FXS {
+    address public constant PLATFOR = 0x000000060e56DEfD94110C1a9497579AD7F5b254;
+}
+
+library BUNNI {
+    address public constant PLATFORM = 0x000000071a273073c824E2a8B0192963e0eEA68b;
+}
