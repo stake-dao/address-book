35ef7a839a92a510aea898c35cfcff0ed3348f55
PumpkingWok <andrearanma@gmail.com>
2023-12-21 12:33:38 +0100

    feat: factory addresses

--- src/strategies/1.sol
+++ src/strategies/1.sol
@@ -4,16 +4,19 @@ pragma solidity >=0.6.0;
 library CRV {
     address public constant STRATEGY = 0x20F1d4Fed24073a9b9d388AfA2735Ac91f079ED6;
     address public constant VE_SDT_FEE_PROXY = 0x9592Ec0605CE232A4ce873C650d2Aa01c79cb69E;
+    address public constant FACTORY = 0xe7D4908e413f4769069b0837c84A5cbC7E4CC124;
 }
 
 library ANGLE {
     address public constant STRATEGY = 0x22635427C72e8b0028FeAE1B5e1957508d9D7CAF;
     address public constant VE_SDT_FEE_PROXY = 0x059F78859a152e42dd590a49bE44F2ebE9FcD1aC;
+    address public constant FACTORY = 0x66f3d3210F84fe8cC2c77A1f001a395b2Ae0B801;
 }
 
 library BAL {
     address public constant STRATEGY = 0x873b031Ea6E4236E44d933Aae5a66AF6d4DA419d;
     address public constant VE_SDT_FEE_PROXY = 0xF94492a9efEE2A6A82256e5794C988D3A711539d;
+    address public constant FACTORY = 0x6e37f0f744377936205610591Eb8787d7bE7946f;
 }
 
 library FXS {
@@ -23,9 +26,11 @@ library FXS {
 
 library YFI {
     address public constant STRATEGY = 0x1be150a35bb8233d092747eBFDc75FB357c35168;
+    address public constant FACTORY = 0x1EFb2C804166be34a6956159646CAE9D0063b7fF;
 }
 
 library PENDLE {
     address public constant STRATEGY = 0xA7641acBc1E85A7eD70ea7bCFFB91afb12AD0c54;
     address public constant VE_SDT_FEE_PROXY = 0x12992595328E52267c95e45B1a97014D6Ddf8683;
+    address public constant FACTORY = 0x4C1CF444Bbbfd3eD6608659B61A1107aF01181e5;
 }
