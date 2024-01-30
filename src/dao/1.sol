ca282162219f0edc1b8bd445b7679e7d8f6db1de
PumpkingWok <andrearanma@gmail.com>
2024-01-30 18:32:04 +0100

    add VESDT_BOOST_PROXY and PROXY_ADMIN

--- src/dao/1.sol
+++ src/dao/1.sol
@@ -5,8 +5,10 @@ library DAO {
     /// Token addresses and revenue sharing.
     address public constant SDT = 0x73968b9a57c6E53d41345FD57a6E6ae27d6CDB2F;
     address public constant VESDT = 0x0C30476f66034E11782938DF8e4384970B6c9e8a;
+    address public constant VESDT_BOOST_PROXY = 0xD67bdBefF01Fc492f1864E61756E5FBB3f173506;
     address public constant VEBOOST = 0x47B3262C96BB55A8D2E4F8E3Fed29D2eAB6dB6e9;
     address public constant FEE_DISTRIBUTOR = 0x29f3dd38dB24d3935CF1bf841e6b2B461A3E5D92;
+    address public constant PROXY_ADMIN = 0xfE612c237A81527a86f2Cac1FD19939CF4F91B9B;
 
     /// SDT Distribution.
     address public constant LOCKER_SDT_DISTRIBUTOR = 0x8Dc551B4f5203b51b5366578F42060666D42AB5E;
