d770aa556db141e211288df6cf9ba65b5b0b1744
PumpkingWok <andrearanma@gmail.com>
2024-02-15 15:07:41 +0100

    feat: add main deployer on bnb dao

--- src/dao/56.sol
+++ src/dao/56.sol
@@ -3,6 +3,7 @@ pragma solidity >=0.6.0;
 
 library DAO {
     address public constant GOVERNANCE = 0xB0552b6860CE5C0202976Db056b5e3Cc4f9CC765;
+    address public constant MAIN_DEPLOYER = 0x000755Fbe4A24d7478bfcFC1E561AfCE82d1ff62;
 
     // Automation
     address public constant ALL_MIGHT = 0x0000000a3Fc396B89e4c11841B39D9dff85a5D05;
