49b135eef17de0bfa0b3ccac969a4f8b5223c763
Laouni <99427751+Lao0ni@users.noreply.github.com>
2024-03-21 18:52:44 +0100

    chore: adding treasury manager address on bnb chain

--- src/dao/56.sol
+++ src/dao/56.sol
@@ -3,6 +3,7 @@ pragma solidity >=0.6.0;
 
 library DAO {
     address public constant GOVERNANCE = 0xB0552b6860CE5C0202976Db056b5e3Cc4f9CC765;
+    address public constant TREASURY_MANAGER = 0xabd75fceC454573cDb89EB7904734B296657c61D;
     address public constant MAIN_DEPLOYER = 0x000755Fbe4A24d7478bfcFC1E561AfCE82d1ff62;
 
     // Automation
