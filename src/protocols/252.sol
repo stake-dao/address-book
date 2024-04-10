6dfd7b0a9ce50ad5cea285fe581d241d52737a29
PumpkingWok <andrearanma@gmail.com>
2024-04-10 11:11:16 +0200

    feat: Frax protocol lib on fraxtal network

---
+++ src/protocols/252.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity >=0.6.0;
+
+library Frax {
+    address public constant FXS = 0xFc00000000000000000000000000000000000002;
+    address public constant VEFXS = 0x54bd5c72645fed784C117cA83533e0584b24Ee5c;
+
+    address public constant FRAX = 0xFc00000000000000000000000000000000000001;
+    address public constant FPIS = 0xfc00000000000000000000000000000000000004;
+}
