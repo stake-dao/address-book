35c3fa8edf24e093295718cbd5a888fd7ac8b0ec
PumpkingWok <andrearanma@gmail.com>
2024-04-24 16:45:30 +0200

    feat: add fraxtal delegation registry

--- src/protocols/252.sol
+++ src/protocols/252.sol
@@ -5,6 +5,7 @@ library Frax {
     address public constant FXS = 0xFc00000000000000000000000000000000000002;
     address public constant VEFXS = 0x007FD070a7E1B0fA1364044a373Ac1339bAD89CF;
     address public constant YIELD_DISTRIBUTOR = 0x39333a540bbea6262e405E1A6d435Bd2e776561E;
+    address public constant DELEGATION_REGISTRY = 0xF5cA906f05cafa944c27c6881bed3DFd3a785b6A;
 
     address public constant FRAX = 0xFc00000000000000000000000000000000000001;
     address public constant FPIS = 0xfc00000000000000000000000000000000000004;
